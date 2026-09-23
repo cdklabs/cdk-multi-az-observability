59155ca0344fe964856b40afe1d119be74cd0ee6
Michael Haken <michael.haken@outlook.com>
2026-09-23 15:29:06 +0100

    fix(auto-approve): reference env vars in approve step to avoid script injection

--- .projenrc.ts
+++ .projenrc.ts
@@ -284,10 +284,6 @@ project.github
   ?.tryFindWorkflow('auto-approve')
   ?.file?.patch(JsonPatch.replace('/jobs/approve/if', "contains(github.event.pull_request.labels.*.name, 'auto-approve') && (github.event.pull_request.user.login == 'cdklabs-automation' || github.event.pull_request.user.login == 'hakenmt' || github.event.pull_request.user.login == 'github-bot' || github.event.pull_request.user.login == 'dependabot[bot]')"));
 
-project.github
-  ?.tryFindWorkflow('auto-approve')
-  ?.file?.patch(JsonPatch.replace('/jobs/approve/steps/0/run', 'gh pr review --approve "${{ github.event.pull_request.number }}" --repo "${{ github.repository }}"'));
-
 // Add corepack enable to package jobs in build workflow
 const corepackStep = { name: 'Enable corepack', run: 'corepack enable' };
 const buildWf = project.github?.tryFindWorkflow('build');
